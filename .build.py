9cfab58ce35c021f242fe0357b2e126311425feb
Liu Y Cheng <xsro@foxmail.com>
2023-09-04 16:47:06 +0800

    SFB vs OFB compare State-feedback control and output-feedback control

--- .build.py
+++ .build.py
@@ -4,6 +4,17 @@ from pathlib import Path
 from subprocess import run
 import sys
 import os
+import argparse
+
+parser = argparse.ArgumentParser(description='script to run all simulation and save figures')
+parser.add_argument("--glob",type=str,default="**/**/main*.py",help="glob pattern to find main.py")
+parser.add_argument("--outpath",type=str,default=r"\\wsl.localhost\Ubuntu-22.04\home\xsr\repo\nlct\simu",help="path to copy all figures to")
+parser.add_argument("--no-copy",action="store_true",help="do not copy figures to outpath")
+parser.add_argument("--no-run",action="store_true",help="do not run simulation")
+args = parser.parse_args()
+
+#获得传入的参数
+print(args)
 
 PIPENV_ENV = os.environ.copy()
 PIPENV_ENV["PIPENV_VERBOSITY"]="-1"
@@ -11,16 +22,22 @@ PIPENV_ENV["PIPENV_VERBOSITY"]="-1"
 PDF_OUT=Path(__file__).parent.joinpath("out")
 PDF_OUT.mkdir(exist_ok=True)
 
-GLOBFILE=sys.argv[1] if len(sys.argv)>1 else "**/**/main*.py"
-OUTPATH=Path(r"\\wsl.localhost\Ubuntu-22.04\home\xsr\repo\nlct\simu")
+GLOBFILE=args.glob
+OUTPATH=Path(args.outpath)
 
 files=glob.glob(GLOBFILE)
 for file in files:
     pybin=Path(sys.orig_argv[0])
     pyscript=Path(file)
-    print("pipenv run python",pyscript)
-    run(["pipenv","run","python",pyscript.absolute()],env=PIPENV_ENV,cwd=pyscript.parent)
+    if args.no_run:
+        print("skip",pyscript)
+        continue
+    else:
+        print("pipenv run python",pyscript)
+        run(["pipenv","run","python",pyscript.absolute()],env=PIPENV_ENV,cwd=pyscript.parent)
     for pdf in glob.glob("*.pdf",root_dir=pyscript.parent):
         filename="_".join(list(pyscript.parts)[0:2]+[pdf])
-        shutil.copy(pyscript.parent.joinpath(pdf),OUTPATH.joinpath(filename))
-        shutil.move(pyscript.parent.joinpath(pdf),PDF_OUT.joinpath(filename))
+        if not args.no_copy:
+            print("\tcopying",pdf)
+            shutil.copy(pyscript.parent.joinpath(pdf),OUTPATH.joinpath(filename))
+            shutil.copy(pyscript.parent.joinpath(pdf),PDF_OUT.joinpath(filename))
---
+++ example/12dot3/main.py
@@ -0,0 +1,137 @@
+#%% example 12.3 
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import solve_ivp
+
+class Model:
+    def __init__(self,epsilon=None,sat=False) -> None:
+        def sat_(x):
+            return np.sign(x)*np.minimum(np.abs(x),1)
+        self.sat = sat_ if sat else lambda x:x
+        self.epsilon = epsilon
+    def system(x:np.ndarray,u:float):
+        x1,x2 = x[0:2].tolist()
+        x1_dot = x2
+        x2_dot = x2**3 + u
+        return np.array([x1_dot,x2_dot])
+    def state_feedback_control(x:np.ndarray):
+        x1,x2 = x.tolist()
+        u = -x2**3-x1-x2
+        return u
+    def state_observer(self,x:np.ndarray):
+        x1,x2 = x[0:2].tolist()
+        xhat1,xhat2 = x[2:4].tolist()
+        y=x1
+        dxhat1 = xhat2 + (2/self.epsilon)*(y-xhat1)
+        dxhat2 = (1/self.epsilon**2)*(y-xhat1)
+        return np.array([dxhat1,dxhat2])
+    def rhs1(self,t,x:np.ndarray):
+        u = Model.state_feedback_control(x)
+        u=self.sat(u)
+        dx = Model.system(x,u)
+        return dx
+    def rhs2(self,t,x:np.ndarray):
+        u = Model.state_feedback_control(x[2:4])
+        u=self.sat(u)
+        dx = Model.system(x,u)
+        dxhat = self.state_observer(x)
+        return np.concatenate([dx,dxhat])
+    
+x0=np.array([0.1,0])
+xhat0=np.array([0,0])
+t_eval=np.linspace(0,10,10000)
+
+model = Model()
+sol1=solve_ivp(model.rhs1,[0,10],x0,t_eval=t_eval)
+model = Model(epsilon=0.1)
+sol2=solve_ivp(model.rhs2,[0,10],np.concatenate([x0,xhat0]),t_eval=t_eval)
+model = Model(epsilon=0.01)
+sol3=solve_ivp(model.rhs2,[0,10],np.concatenate([x0,xhat0]),t_eval=t_eval)
+model = Model(epsilon=0.005)
+sol4=solve_ivp(model.rhs2,[0,10],np.concatenate([x0,xhat0]),t_eval=t_eval)
+#%% compare SFB and OFB
+plt.figure()
+plt.subplot(3,1,1)
+plt.plot(sol1.t,sol1.y[0,:],'-')
+plt.plot(sol2.t,sol2.y[0,:],'--')
+plt.plot(sol3.t,sol3.y[0,:],'--')
+plt.plot(sol4.t,sol4.y[0,:],'--')
+plt.ylabel("$x_1$")
+plt.subplot(3,1,2)
+plt.plot(sol1.t,sol1.y[1,:])
+plt.plot(sol2.t,sol2.y[1,:],'--')
+plt.plot(sol3.t,sol3.y[1,:],'--')
+plt.plot(sol4.t,sol4.y[1,:],'--')
+plt.ylabel("$x_2$")
+  
+plt.subplot(3,1,3)
+def generate_u(sol):
+    return np.array([Model.state_feedback_control(sol.y[-2:,j]) for j in range(sol.y.shape[1])])
+u={k:generate_u(v) for k,v in {"1":sol1,"2":sol2,"3":sol3, "4":sol4}.items()}
+plt.plot(sol1.t,u["1"])
+plt.plot(sol2.t,u["2"],'--')
+plt.plot(sol3.t,u["3"],'--')
+plt.plot(sol4.t,u["4"],'--')
+plt.ylabel("$u$")
+plt.xlim(0,0.1)
+plt.tight_layout()
+plt.legend([
+    "SFB","OFB $\epsilon=0.1$","OFB $\epsilon=0.01$","OFB $\epsilon=0.005$"
+    ])
+plt.savefig("SFB_vs_OFB.pdf")
+# %% Simuate OFB with epsilon=0.004
+model = Model(epsilon=0.004)
+sol5=solve_ivp(model.rhs2,[0,10],np.concatenate([x0,xhat0]),t_eval=t_eval)
+plt.figure()
+plt.subplot(3,1,1)
+plt.plot(sol5.t,sol5.y[0,:],'-')
+plt.subplot(3,1,2)
+plt.plot(sol5.t,sol5.y[1,:],'-')
+plt.subplot(3,1,3)
+plt.plot(sol5.t,generate_u(sol5),'-')
+plt.savefig("OFB_epsilon_0.004.pdf")
+# %% OFB with saturation
+model1 = Model(sat=True)
+model2 = Model(epsilon=0.1,sat=True)
+model3 = Model(epsilon=0.01,sat=True)
+model4 = Model(epsilon=0.001,sat=True)
+models={"2":model2,"3":model3, "4":model4}
+sols={"1":solve_ivp(model1.rhs1,[0,10],x0,t_eval=t_eval)}
+for k,v in models.items():
+    sols[k]=solve_ivp(v.rhs2,[0,10],np.concatenate([x0,xhat0]),t_eval=t_eval)
+#%% Plot 
+plt.figure()
+plt.subplot(3,1,1)
+plt.plot(sols['1'].t,sols['1'].y[0,:],'-')
+plt.plot(sols['2'].t,sols['2'].y[0,:],'--')
+plt.plot(sols['3'].t,sols['3'].y[0,:],'--')
+plt.plot(sols['4'].t,sols['4'].y[0,:],'--')
+plt.ylabel("$x_1$")
+plt.legend([
+    "SFB","OFB $\epsilon=0.1$","OFB $\epsilon=0.01$","OFB $\epsilon=0.001$"
+    ],loc="upper right")
+plt.subplot(3,1,2)
+plt.plot(sols['1'].t,sols['1'].y[1,:])
+plt.plot(sols['2'].t,sols['2'].y[1,:],'--')
+plt.plot(sols['3'].t,sols['3'].y[1,:],'--')
+plt.plot(sols['4'].t,sols['4'].y[1,:],'--')
+plt.ylabel("$x_2$")
+  
+plt.subplot(3,1,3)
+def generate_u(sol):
+    u=np.zeros((sol.y.shape[1],))
+    for i in range(sol.y.shape[1]):
+        u[i] = Model.state_feedback_control(sol.y[-2:,i])
+        u[i]=model1.sat(u[i])
+    return u
+plt.plot(sols['1'].t,generate_u(sols['1']))
+plt.plot(sols['2'].t,generate_u(sols['2']),'--')
+plt.plot(sols['3'].t,generate_u(sols['3']),'--')
+plt.plot(sols['4'].t,generate_u(sols['4']),'--')
+plt.ylabel("$u$")
+plt.xlim(0,0.1)
+plt.tight_layout()
+
+plt.savefig("SFB_vs_OFB_sat.pdf")
+
+# %%

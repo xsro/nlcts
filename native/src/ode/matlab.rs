b65ba5b0ece2a082bdb1a1caad67e7dd924766e0
Liu Y Cheng <xsro@foxmail.com>
2023-10-02 17:45:42 +0800

    maybe use a Array2 to store all data is better

--- native/src/ode/matlab.rs
+++ native/src/ode/matlab.rs
@@ -1,3 +1,30 @@
-fn ode2(){
-    
+use std::collections::BTreeMap;
+
+use ndarray::Array1;
+use num::Float;
+use super::{RK4,Euler};
+
+pub fn collect_ode_result<T>(
+    t:Vec<T>,states:Vec<Array1<T>>,
+    signals:Vec<Option<BTreeMap<String,T>>>
+){
+
+}
+
+
+pub fn ode2<T,F>(rhs:F,t:Array1<T>,initial:Array1<T>)->Result<(),String>
+where 
+    T:Float,
+    F:for<'a> FnMut(&'a T,&'a Array1<T>)->(Array1<T>,Option<BTreeMap<String,T>>)
+{
+    if t.len()<2{
+        return Err("third parameter `t` should has at least two element".to_string());
+    }
+    let mut solver=Euler::new(rhs,initial,t[1]-t[0]);
+    solver.step();
+    for i in 1..t.len()-1{
+        solver.step_size=t[i]-t[i-1];
+        solver.step();
+    }
+    Ok(())
 }
